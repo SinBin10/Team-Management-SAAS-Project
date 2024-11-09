d479a83deaafe49a7c7c7279a1509872a98e0032
SinBin10 <bincode10@gmail.com>
2024-11-09 23:06:08 +0530

    making the folder structure for controllers(functions which say what to do when client visits on a particular route)

---
+++ server/controllers/auth-controller.js
@@ -0,0 +1,6 @@
+const login = () => {};
+const register = () => {
+  console.log("user registered");
+};
+
+module.exports = { login, register };
--- server/routers/auth-router.js
+++ server/routers/auth-router.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { register } = require("../controllers/auth-controller");
 
 router.route("/login").get();
-router.route("/register").post();
+router.route("/register").post(register);
+
+module.exports = router;
