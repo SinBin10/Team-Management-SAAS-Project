602df37c496b7ec7eb132b37a5cfaaf525c023ef
SinBin10 <bincode10@gmail.com>
2024-11-09 22:19:44 +0530

    making router folder structure.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
+const router = require("./routers/auth-router");
 
-app.get("/", (req, res) => {
-  res.send("working good...");
-});
+app.use("/", router);
 
 app.listen(3000, () => {
   console.log("hello");
---
+++ server/routers/auth-router.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+
+router.route("/login").get();
+router.route("/register").post();
